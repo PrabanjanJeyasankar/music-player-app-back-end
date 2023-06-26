7acec5c14c529e90c4c33a21b45a31c1137a4163
Prabanjan Jeyasankar <prabanjanjeyasankar@gmail.com>
2023-06-26 13:37:13 +0530

    Fix. Fixed MVC Architecture.

--- middlewares/fileupload.js
+++ middlewares/fileupload.js
@@ -1,1 +1,15 @@
-// FILEUPLOAD
+const multer = require('multer')
+
+
+const storage = multer.diskStorage({
+    filename: function(request, file, callback){
+        callback(null, file.originalname)
+    },
+    destination: function(request, file, callback)
+    {
+        callback(null, './assets')
+    },
+})
+
+const upload = multer({storage})
+module.exports = upload
--- models/fileModel.js
+++ models/fileModel.js
@@ -1,1 +1,41 @@
-// filemodle
+const mongoose = require('mongoose')
+
+const fileSchema = new mongoose.Schema(
+    {
+        originalname:{
+            type:String,
+            required:true,
+            unique:true,
+            index:true
+        },
+        size:{
+            type:Number,
+            required:true
+        },
+        mimetype:{
+            type:String,
+            required:true
+        },
+        destination:{
+            type:String,
+            required:true
+        },
+        filename: {
+            type:String,
+            required:true,
+            unique:true,
+            index:true
+        },
+        filePath:{
+            type:String,
+            required:true,
+            unique: true,
+            index:true
+        }
+    },
+    {
+        collection:'files'
+    }
+)
+
+module.exports = mongoose.model('files', fileSchema)
